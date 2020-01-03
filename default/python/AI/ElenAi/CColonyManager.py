0b1b99b983fcc9d81c828d4b529a523f2d9cf8c7
Alexander Schuch <aschuch247@gmail.com>
2020-01-03 17:06:14 +0000

    Update the colony manager.
    The manager now handles 'BLD_AUTO_HISTORY_ANALYSER'.

--- default/python/AI/ElenAi/CColonyManager.py
+++ default/python/AI/ElenAi/CColonyManager.py
@@ -3,6 +3,7 @@ This is the colony and outpost manager.
 """
 
 from ElenAi.CManager import CManager
+from ElenAi.CProductionQueue import CProductionQueue
 
 
 class CColonyManager(CManager):
@@ -13,4 +14,57 @@ class CColonyManager(CManager):
 
 
     def vManage(self):
-        pass
+        for ixSystem in self.tixGetOwnSystem():
+            self.vManageSystem(ixSystem)
+
+
+    def bIsOwn(self, oUniverseObject):
+        return oUniverseObject.ownedBy(self.fo.empireID())
+
+
+    def tixGetOwnSystem(self):
+        oFoUniverse = self.fo.getUniverse()
+
+        for ixSystem in oFoUniverse.systemIDs:
+            oFoSystem = oFoUniverse.getSystem(ixSystem)
+
+            for ixPlanet in oFoSystem.planetIDs:
+                oFoPlanet = oFoUniverse.getPlanet(ixPlanet)
+
+                if (self.bIsOwn(oFoPlanet)):
+                    yield ixSystem
+                    break
+
+
+    def vConditionallyAddBuilding(self, ixPlanet, sBuilding):
+        oFoUniverse = self.fo.getUniverse()
+        oProductionQueue = CProductionQueue(self.fo)
+
+        bHasBuilding = False
+
+        for ixBuilding in oFoUniverse.getPlanet(ixPlanet).buildingIDs:
+            oFoBuilding = oFoUniverse.getBuilding(ixBuilding)
+
+            if (oFoBuilding.buildingTypeName == sBuilding):
+                bHasBuilding = True
+                break
+
+        if (not bHasBuilding):
+            if (not oProductionQueue.bIsEnqueuedBuilding(ixPlanet, sBuilding)):
+                oProductionQueue.vEnqueueBuilding(ixPlanet, sBuilding)
+
+
+    def vManageSystem(self, ixSystem):
+        oFoUniverse = self.fo.getUniverse()
+        oFoSystem = oFoUniverse.getSystem(ixSystem)
+        oProductionQueue = CProductionQueue(self.fo)
+
+        for ixPlanet in oFoSystem.planetIDs:
+            oFoPlanet = oFoUniverse.getPlanet(ixPlanet)
+
+            if (self.bIsOwn(oFoPlanet)):
+                for ixBuilding in oFoPlanet.buildingIDs:
+                    oFoBuilding = oFoUniverse.getBuilding(ixBuilding)
+
+                    if (oFoBuilding.buildingTypeName == 'BLD_IMPERIAL_PALACE'):
+                        self.vConditionallyAddBuilding(ixPlanet, 'BLD_AUTO_HISTORY_ANALYSER')

1f08b4ed50bfa01f05ade37e46a6db264308202b
Alexander Schuch <aschuch247@gmail.com>
2020-01-03 11:53:54 +0000

    Explore unexplored systems with scouts.

--- default/python/AI/ElenAi/CUniverseAssessment.py
+++ default/python/AI/ElenAi/CUniverseAssessment.py
@@ -2,12 +2,36 @@
 Assess the universe.
 """
 
+from ElenAi.CSystem import CSystem
+from ElenAi.CUniverse import CUniverse
+
 
 class CUniverseAssessment(object):
+
+
     def __init__(self, fo):
         self.fo = fo
 
+
+    def tixGetIdleScoutShip(self):
+        oFoUniverse = self.fo.getUniverse()
+
+        for ixFleet in oFoUniverse.fleetIDs:
+            oFoFleet = oFoUniverse.getFleet(ixFleet)
+
+            if oFoFleet.ownedBy(self.fo.empireID()):
+                if (oFoFleet.finalDestinationID == -1) or (oFoFleet.finalDestinationID == oFoFleet.systemID):
+                    for ixShip in oFoFleet.shipIDs:
+                        oFoShip = oFoUniverse.getShip(ixShip)
+                        oFoShipDesign = oFoShip.design
+
+                        for parts in oFoShipDesign.parts:
+                            if (parts == 'DT_DETECTOR_1'):
+                                yield ixShip
+
+
     def vAssessUniverse(self):
+        oUniverse = CUniverse()
         oFoUniverse = self.fo.getUniverse()
 
         for ixSystem in oFoUniverse.systemIDs:
@@ -17,6 +41,12 @@ class CUniverseAssessment(object):
             if (oFoSystem.numWormholes != 0):
                 print 'Wormholes found!'
 
+            oUniverse.vAddSystem(ixSystem, CSystem(oFoSystem.x, oFoSystem.y))
+
+        for ixSystem in oFoUniverse.systemIDs:
+            for ixSystemNeighbour in oFoUniverse.getImmediateNeighbors(ixSystem, self.fo.empireID()):
+                oUniverse.vLinkSystem(ixSystem, ixSystemNeighbour)
+
         oFoEmpire = self.fo.getEmpire()
 
         for ixSystem in oFoEmpire.exploredSystemIDs:
@@ -25,23 +55,21 @@ class CUniverseAssessment(object):
 
         # Get a list of known systems that have not been explored yet.
 
-        unexploredSystemId = -1;
+        setSystemUnexplored = set(oFoUniverse.systemIDs).difference(oFoEmpire.exploredSystemIDs)
+        setSystemUnexploredTargeted = set()
 
-        for ixSystem in set(oFoUniverse.systemIDs).difference(oFoEmpire.exploredSystemIDs):
-            oFoSystem = oFoUniverse.getSystem(ixSystem)
-            # print 'I have not yet explored system %s (%d).' % (oFoSystem.name, oFoSystem.systemID)
-            unexploredSystemId = ixSystem
+        for ixShip in self.tixGetIdleScoutShip():
+            oFoShip = oFoUniverse.getShip(ixShip)
 
-        if unexploredSystemId != -1:
+            # The scout ship is supposed to be located in a system, not moving.
 
-            # This code will send all idle fleets to the same system. Mark a system with the proper incoming fleet.
+            oGraphRouter = oUniverse.oGetGraphRouter(oFoShip.systemID)
 
-            for ixFleet in oFoUniverse.fleetIDs:
-                oFoFleet = oFoUniverse.getFleet(ixFleet)
+            # Only consider unexplored systems which are not yet targeted by scouts.
 
-                if oFoFleet.ownedBy(self.fo.empireID()):
-                    if (oFoFleet.finalDestinationID == -1) or (oFoFleet.finalDestinationID == oFoFleet.systemID):
+            ixSystemClosestUnexplored = oGraphRouter.ixGetClosestGraphNode(setSystemUnexplored.difference(setSystemUnexploredTargeted))
 
-                        # The fleet has no orders yet or any more.
+            setSystemUnexploredTargeted.add(ixSystemClosestUnexplored)
 
-                        self.fo.issueFleetMoveOrder(ixFleet, unexploredSystemId)
+            self.fo.issueNewFleetOrder(oFoShip.design.name, ixShip)
+            self.fo.issueFleetMoveOrder(oFoShip.fleetID, ixSystemClosestUnexplored)

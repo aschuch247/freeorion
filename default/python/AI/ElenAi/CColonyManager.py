87e49cfb729f4cba63f9c4aa9d1cc21ddf3a46df
Alexander Schuch <aschuch247@gmail.com>
2020-01-03 18:35:53 +0000

    Add support for more buildings.
    - BLD_GENOME_BANK
    - BLD_MEGALITH

--- default/python/AI/ElenAi/CColonyManager.py
+++ default/python/AI/ElenAi/CColonyManager.py
@@ -69,5 +69,12 @@ class CColonyManager(CManager):
                 for ixBuilding in oFoPlanet.buildingIDs:
                     oFoBuilding = oFoUniverse.getBuilding(ixBuilding)
 
+                    # @todo If the whole empire has no BLD_IMPERIAL_PALACE, build a new one!
+
                     if (oFoBuilding.buildingTypeName == 'BLD_IMPERIAL_PALACE'):
                         self.vConditionallyAddBuilding(ixPlanet, 'BLD_AUTO_HISTORY_ANALYSER')
+                        self.vConditionallyAddBuilding(ixPlanet, 'BLD_MEGALITH')
+
+                        # @todo Also build BLD_GENOME_BANK redundantly.
+
+                        self.vConditionallyAddBuilding(ixPlanet, 'BLD_GENOME_BANK')

dc1885694e5150bec11f65cd074e7a09a71d71de
Alexander Schuch <aschuch247@gmail.com>
2020-02-02 23:17:48 +0000

    Update method to predict colony size.

--- default/python/AI/ElenAi/CColonisationManager.py
+++ default/python/AI/ElenAi/CColonisationManager.py
@@ -3,6 +3,11 @@ The colonisation manager assesses the universe for habitable planets.
 """
 
 # @todo We need a method to rate planets based on the system and the planet itself.
+# - Prefer systems with many planets for easier defence.
+# - Prefer systems with black hole or red star (can be converted to black hole) for ship production and the black hole
+#   power generator. Also for better research.
+# - Consider neutron stars for better research and BLD_NEUTRONIUM_EXTRACTOR.
+# - Consider white and blue stars for PHOTOTROPHIC species.
 
 
 from ElenAi.CManager import CManager
@@ -56,8 +61,22 @@ class CColonisationManager(CManager):
         Return None in case the planet is uninhabitable.
         """
 
-        TARGET_POPULATION_BEFORE_SCALING_PRIORITY = 10
-        TARGET_POPULATION_AFTER_SCALING_PRIORITY  = 14
+        bConsiderFullTerraforming = False
+
+        # @todo Consider support for:
+        # - BLD_HYPER_DAM
+        # - DIM_RIFT_MASTER_SPECIAL
+        # - HOMEWORLD_GROWTH_FOCUS_BOOST
+        # - SP_LEMBALALAM
+        # - TEMPORAL_ANOMALY_SPECIAL
+        # - TIDAL_LOCK_SPECIAL
+        # - XENOPHOBIC
+
+        TARGET_POPULATION_BEFORE_SCALING_PRIORITY       = 10
+        TARGET_POPULATION_SCALING_PRIORITY              = 12
+        TARGET_POPULATION_AFTER_SCALING_PRIORITY        = 14
+        TARGET_POPULATION_LAST_BEFORE_OVERRIDE_PRIORITY = 16
+        TARGET_POPULATION_OVERRIDE_PRIORITY             = 17
 
         HabitableSizeModifier = {
             TARGET_POPULATION_BEFORE_SCALING_PRIORITY: {
@@ -105,6 +124,13 @@ class CColonisationManager(CManager):
         }
 
 
+        def fAddPopulationBonus(sName, fPopulationBonus, bPrintCalculation = False):
+            if (bPrintCalculation):
+                print '%s: %+.2f' % (sName, fPopulationBonus)
+
+            return fPopulationBonus
+
+
         def iGetHabitableSizeModifier(iPriority, bPrintCalculation = False):
             iModifier = 0
 
@@ -112,15 +138,15 @@ class CColonisationManager(CManager):
                 if (iDictPriority == iPriority):
                     for sTechnology, iPlanetEnvironmentDict in TechnologyDict.items():
                         if (sTechnology in sAvailableTechFrozenset):
-                            if (oFoPlanet.type in iPlanetEnvironmentDict):
-                                iModifier += iPlanetEnvironmentDict[oFoPlanet.type]
+                            if (oFoPlanetEnvironment in iPlanetEnvironmentDict):
+                                iModifier += iPlanetEnvironmentDict[oFoPlanetEnvironment]
 
                                 if (bPrintCalculation):
-                                    print 'priority %d, technology \'%s\', planet environment %d, modifier %d' % (
+                                    print 'priority %d, technology \'%s\', planet environment %d, modifier %+d' % (
                                         iPriority,
                                         sTechnology,
-                                        oFoPlanet.type,
-                                        iPlanetEnvironmentDict[oFoPlanet.type]
+                                        oFoPlanetEnvironment,
+                                        iPlanetEnvironmentDict[oFoPlanetEnvironment]
                                     )
 
             return iModifier
@@ -131,6 +157,8 @@ class CColonisationManager(CManager):
 
         if (bPrintCalculation):
             print 'Planet %d population calculation for \'%s\':' % (oFoPlanet.id, sSpecies)
+
+        # ENVIRONMENT_MODIFIER - TARGET_POPULATION_OVERRIDE_PRIORITY
 
         if (oFoPlanetEnvironment == self.fo.planetEnvironment.uninhabitable):
             if (bPrintCalculation):
@@ -145,25 +173,88 @@ class CColonisationManager(CManager):
             return None
 
         fHabitableSize = oFoPlanet.habitableSize
-        fMaximumPopulation = fHabitableSize
-
-        if (bPrintCalculation):
-            print 'habitable size %.2f' % (fHabitableSize)
+        fMaximumPopulation = 0.0
 
         sAvailableTechFrozenset = self.fo.getEmpire().availableTechs
+        sSpeciesTagFrozenset = frozenset(oFoSpecies.tags)
+        sPlanetSpecialsFrozenset = frozenset(oFoPlanet.specials)
 
         fMaximumPopulation += iGetHabitableSizeModifier(TARGET_POPULATION_BEFORE_SCALING_PRIORITY, bPrintCalculation) * fHabitableSize
+
+        # ENVIRONMENT_MODIFIER - TARGET_POPULATION_BEFORE_SCALING_PRIORITY
+
+        EnvironmentalModifier = {
+            self.fo.planetEnvironment.hostile: -4.0,
+            self.fo.planetEnvironment.poor:    -2.0,
+            self.fo.planetEnvironment.good:     3.0
+        }
+
+        fMaximumPopulation += fAddPopulationBonus('ENVIRONMENT_MODIFIER', EnvironmentalModifier.get(oFoPlanetEnvironment, 0) * fHabitableSize, bPrintCalculation)
+
+        # BAD_POPULATION - TARGET_POPULATION_SCALING_PRIORITY
+
+        if ('BAD_POPULATION' in sSpeciesTagFrozenset):
+            fMaximumPopulation += fAddPopulationBonus('BAD_POPULATION', -0.25 * abs(fMaximumPopulation), bPrintCalculation)
+
+        # GOOD_POPULATION - TARGET_POPULATION_SCALING_PRIORITY
+
+        if ('GOOD_POPULATION' in sSpeciesTagFrozenset):
+            fMaximumPopulation += fAddPopulationBonus('GOOD_POPULATION', 0.25 * abs(fMaximumPopulation), bPrintCalculation)
+
+        # GASEOUS_BONUS - TARGET_POPULATION_SCALING_PRIORITY
+        # @todo This calculation probably is wrong for a mix of GOOD_POPULATION and GASEOUS.
+
+        if (('GASEOUS' in sSpeciesTagFrozenset) and (oFoPlanet.type == self.fo.planetType.gasGiant)):
+            fMaximumPopulation += fAddPopulationBonus('GASEOUS_BONUS', -0.5 * abs(fMaximumPopulation), bPrintCalculation)
+
         fMaximumPopulation += iGetHabitableSizeModifier(TARGET_POPULATION_AFTER_SCALING_PRIORITY, bPrintCalculation) * fHabitableSize
+
+        # GRO_PLANET_ECOL - TARGET_POPULATION_AFTER_SCALING_PRIORITY
 
         if (
             ('GRO_PLANET_ECOL' in sAvailableTechFrozenset) and
             (not 'GRO_SYMBIOTIC_BIO' in sAvailableTechFrozenset)
         ):
             if (oFoPlanetEnvironment in [self.fo.planetEnvironment.adequate, self.fo.planetEnvironment.good]):
-                fMaximumPopulation += 1
+                fMaximumPopulation += fAddPopulationBonus('GRO_PLANET_ECOL', 1.0, bPrintCalculation)
 
-                if (bPrintCalculation):
-                    print '\'GRO_PLANET_ECOL\': +1'
+        # HOMEWORLD_BONUS_POPULATION - TARGET_POPULATION_AFTER_SCALING_PRIORITY
+
+        if (oFoPlanet.id in oFoSpecies.homeworlds):
+            fMaximumPopulation += fAddPopulationBonus('HOMEWORLD_BONUS_POPULATION', 2.0 * fHabitableSize, bPrintCalculation)
+
+        # @todo HOMEWORLD_GROWTH_FOCUS_BOOST - TARGET_POPULATION_AFTER_SCALING_PRIORITY
+
+        # SELF_SUSTAINING_BONUS - TARGET_POPULATION_AFTER_SCALING_PRIORITY
+
+        if ('SELF_SUSTAINING' in sSpeciesTagFrozenset):
+            fMaximumPopulation += fAddPopulationBonus('SELF_SUSTAINING_BONUS', 3.0 * fHabitableSize, bPrintCalculation)
+
+        # GAIA_SPECIAL - TARGET_POPULATION_AFTER_SCALING_PRIORITY
+
+        if ('GAIA_SPECIAL' in sPlanetSpecialsFrozenset):
+
+            # SP_EXOBOT does not have any good planet environment.
+
+            if ((bConsiderFullTerraforming and (sSpecies != 'SP_EXOBOT')) or (oFoPlanetEnvironment == self.fo.planetEnvironment.good)):
+                fMaximumPopulation += fAddPopulationBonus('GAIA_SPECIAL', 3.0 * fHabitableSize, bPrintCalculation)
+
+        # PHOTOTROPHIC_BONUS - TARGET_POPULATION_LAST_BEFORE_OVERRIDE_PRIORITY
+
+        if ('PHOTOTROPHIC' in sSpeciesTagFrozenset):
+            iStarType = self.fo.getUniverse().getSystem(oFoPlanet.systemID).starType
+
+            if (fMaximumPopulation >= 0.0):
+                PhototrophicModifier = {
+                    self.fo.starType.blue:        3.0,
+                    self.fo.starType.white:       1.5,
+                    self.fo.starType.red:        -1.0,
+                    self.fo.starType.neutron:    -1.0,
+                    self.fo.starType.blackHole: -10.0,
+                    self.fo.starType.noStar:    -10.0
+                }
+
+                fMaximumPopulation += fAddPopulationBonus('PHOTOTROPHIC_BONUS', PhototrophicModifier.get(iStarType, 0.0) * fHabitableSize, bPrintCalculation)
 
         return fMaximumPopulation
 

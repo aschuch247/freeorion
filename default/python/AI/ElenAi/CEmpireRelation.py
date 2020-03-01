74874eea45cb3c94f7e9dd63368c09dcacd50467
Alexander Schuch <aschuch247@gmail.com>
2020-03-01 18:42:17 +0000

    Add methods to check a fleet's relation.
    These methods checks the relation of the empire of a fleet to the own
    empire (war, peace, alliance).

--- default/python/AI/ElenAi/CEmpireRelation.py
+++ default/python/AI/ElenAi/CEmpireRelation.py
@@ -7,9 +7,8 @@ class CEmpireRelation(object):
 
 
     def __init__(self, ixOwnEmpire):
-        """
-        @todo Add information about war, peace, and alliances.
-        """
+
+        # @todo Add information about war, peace, and alliances.
 
         self.__m_ixOwnEmpire = ixOwnEmpire
 
@@ -24,11 +23,47 @@ class CEmpireRelation(object):
 
     def bIsFriendlyPlanet(self, oPlanet):
         """
-        Check whether the planet belongs to any friendly empire or any allied empire.
+        Check whether the planet belongs to our own empire, or to any empire at peace (including allies).
         """
 
-        return self.bIsOwnPlanet()
+        # @todo Implement!
+
+        return self.bIsOwnPlanet(oPlanet)
+
+
+    def bIsAlliedPlanet(self, oPlanet):
+        """
+        Check whether the planet belongs to our own empire, or to any allied empire.
+        """
+
+        # @todo Implement!
+
+        return self.bIsOwnPlanet(oPlanet)
 
 
     def bIsOwnFleet(self, oFleet):
+        """
+        Check whether the fleet belongs to our own empire.
+        """
+
         return oFleet.ixGetEmpire() == self.__m_ixOwnEmpire
+
+
+    def bIsFriendlyFleet(self, oFleet):
+        """
+        Check whether the fleet belongs to our own empire, or to any empire at peace (including allies).
+        """
+
+        # @todo Implement!
+
+        return self.bIsOwnFleet(oFleet)
+
+
+    def bIsAlliedFleet(self, oFleet):
+        """
+        Check whether the fleet belongs to our own empire, or to any allied empire.
+        """
+
+        # @todo Implement!
+
+        return self.bIsOwnFleet(oFleet)

65011c71c81496cda87da264774adad1f5e91b52
Alexander Schuch <aschuch247@gmail.com>
2021-02-07 19:42:00 +0000

    Add fleet predictor unit test.

---
+++ default/python/AI/ElenAi/tests/CFleetPredictorTest.py
@@ -0,0 +1,58 @@
+import unittest
+
+from ElenAi.CFleet import CFleet
+from ElenAi.CFleetPredictor import CFleetPredictor
+from ElenAi.CShip import CShip
+
+
+class CFleetPredictorTest(unittest.TestCase):
+
+
+    def test_CFleetPredictor_InitialFleet(self):
+        oFleet = CFleet(1, 1, 1, 1, 0.0)
+
+        oFleet.vAddShip(
+            CShip(
+                1,
+                'SP_SCYLIOR',
+                'SH_BASIC_SMALL',
+                ['DT_DETECTOR_1']
+            )
+        )
+
+        oFleet.vAddShip(
+            CShip(
+                2,
+                'SP_SCYLIOR',
+                'SH_BASIC_SMALL',
+                ['DT_DETECTOR_1']
+            )
+        )
+
+        oFleet.vAddShip(
+            CShip(
+                3,
+                'SP_SCYLIOR',
+                'SH_BASIC_MEDIUM',
+                ['SR_WEAPON_1_1', 'AR_STD_PLATE', 'FU_BASIC_TANK']
+            )
+        )
+
+        oFleet.vAddShip(
+            CShip(
+                4,
+                'SP_SCYLIOR',
+                'SH_BASIC_MEDIUM',
+                ['CO_COLONY_POD']
+            )
+        )
+
+        oFleetPredictor = CFleetPredictor(oFleet)
+
+        self.assertEqual(True, oFleetPredictor.bIsArmed())
+        self.assertEqual(3.0, oFleetPredictor.fGetDamage())
+        self.assertEqual(50.0, oFleetPredictor.fGetMaxDetection())
+        self.assertEqual(0.0, oFleetPredictor.fGetMaxShield())
+        self.assertEqual(36.0, oFleetPredictor.fGetMaxStructure())
+        self.assertEqual(60.0, oFleetPredictor.fGetSpeed())
+        self.assertEqual(5.0, oFleetPredictor.fGetMinStealth())
